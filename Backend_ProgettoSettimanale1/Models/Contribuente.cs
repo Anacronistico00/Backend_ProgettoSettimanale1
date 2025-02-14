86d6651baab86c7dc84f86cdc6bcb8e3301b4b00
Vittorio00 <viktoryturiaci2323@gmail.com>
2025-02-14 10:04:36 +0100

    Aggiungere i file di progetto.

---
+++ Backend_ProgettoSettimanale1/Models/Contribuente.cs
@@ -0,0 +1,59 @@
+﻿namespace Backend_ProgettoSettimanale1.Models
+{
+    public class Contribuente
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public DateTime DataNascita { get; set; }
+        public string CodiceFiscale { get; set; }
+        public char Sesso { get; set; }
+        public string ComuneResidenza { get; set; }
+        public decimal RedditoAnnuale { get; set; }
+
+        public Contribuente(string nome, string cognome, DateTime dataNascita, string codiceFiscale, char sesso, string comuneResidenza, decimal redditoAnnuale)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            DataNascita = dataNascita;
+            CodiceFiscale = codiceFiscale;
+            Sesso = sesso;
+            ComuneResidenza = comuneResidenza;
+            RedditoAnnuale = redditoAnnuale;
+        }
+
+        public decimal CalcoloImposta()
+        { 
+            decimal imposta = 0;
+
+            switch (RedditoAnnuale)
+            {
+                case <= 15000:
+                    imposta = RedditoAnnuale * 0.23m;
+                    break;
+                case <= 28000:
+                    imposta = 3450 + (RedditoAnnuale - 15000) * 0.27m;
+                    break;
+                case <= 55000:
+                    imposta = 6960 + (RedditoAnnuale - 28000) * 0.38m;
+                    break;
+                case <= 75000:
+                    imposta = 17220 + (RedditoAnnuale - 55000) * 0.41m;
+                    break;
+                default:
+                    imposta = 25420 + (RedditoAnnuale - 75000) * 0.43m;
+                    break;
+            }
+
+            return imposta;
+        }
+        public override string ToString()
+        {
+            return $"Contribuente: {Nome} {Cognome},\n" +
+                   $"nato il {DataNascita:dd/MM/yyyy} ({Sesso}),\n" +
+                   $"residente a {ComuneResidenza},\n" +
+                   $"codice fiscale: {CodiceFiscale}\n\n" +
+                   $"Reddito dichiarato: {RedditoAnnuale.ToString("N2")}\n" +
+                   $"IMPOSTA DA VERSARE: Eur {CalcoloImposta().ToString("N2")}";
+        }
+    }
+}
---
+++ Backend_ProgettoSettimanale1/Program.cs
@@ -0,0 +1,54 @@
+﻿using Backend_ProgettoSettimanale1.Models;
+
+Console.Write("Inserisci il tuo nome: ");
+string nome = Console.ReadLine();
+
+Console.Write("Inserisci il tuo cognome: ");
+string cognome = Console.ReadLine();
+
+DateTime dataDiNascita;
+while (true)
+{
+    Console.Write("Inserisci la tua data di nascita (gg-mm-yyyy): ");
+    if (DateTime.TryParse(Console.ReadLine(), out dataDiNascita))
+    {
+        break;
+    }
+    Console.WriteLine("Data non valida, riprova.");
+}
+
+char sesso;
+
+while (true)
+{
+    Console.Write("Inserisci il tuo sesso (M/F): ");
+    if (char.TryParse(Console.ReadLine(), out sesso) && (sesso == 'M' || sesso == 'F'))
+    {
+        break;
+    }
+    Console.WriteLine("Sesso non valido, inserire M o F.");
+}
+
+Console.Write("Inserisci il tuo codice fiscale: ");
+string codiceFiscale = Console.ReadLine();
+
+Console.Write("Inserisci il tuo comune di residenza: ");
+string comuneResidenza = Console.ReadLine();
+
+decimal redditoAnnuale;
+
+while (true)
+{
+    Console.Write("Inserisci il reddito annuale: ");
+    if (decimal.TryParse(Console.ReadLine(), out redditoAnnuale) && redditoAnnuale > 0)
+    {
+        break;
+    }
+    Console.WriteLine("Reddito non valido, riprova.");
+}
+
+Contribuente contribuente = new Contribuente(nome, cognome, dataDiNascita, codiceFiscale, sesso, comuneResidenza, redditoAnnuale);
+
+Console.WriteLine("==================================================");
+Console.WriteLine("\nCALCOLO DELL'IMPOSTA DA VERSARE:\n");
+Console.WriteLine(contribuente);
